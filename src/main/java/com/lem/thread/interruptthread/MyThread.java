66a6afd14f7c3074429e0bea8531e01f0a1335fc
lemonHQJ <694162060@qq.com>
2017-07-18 10:18:00 +0800

    lala

--- src/main/java/com/lem/thread/interruptthread/MyThread.java
+++ src/main/java/com/lem/thread/interruptthread/MyThread.java
@@ -4,15 +4,8 @@ public class MyThread extends Thread{
 
 	@Override
 	public void run() {
-		for(int i=0;i<5000;i++){
+		for(int i=0;i<50000;i++){
 			System.out.println("i= " + (i+1));
-			if(i==100){
-				for(int j=0;j<1000;j++){
-					System.out.println("~~~~~~~~");
-					System.out.println(Thread.currentThread().getName());
-				}	
-			}
-
 		}
 	}
 }
