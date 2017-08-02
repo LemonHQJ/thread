37a63175879ef94a5fb9b5c21fab97b4c87befde
lemonHQJ <694162060@qq.com>
2017-08-02 17:58:58 +0800

    10

--- src/main/java/com/lem/thread3/p_r_test/Main.java
+++ src/main/java/com/lem/thread3/p_r_test/Main.java
@@ -3,7 +3,19 @@ package com.lem.thread3.p_r_test;
 public class Main {
 
 	public static void main(String[] args) {
-
+		demo1();
+	}
+	
+	public static void demo1(){
+		String str = new String("");
+		C c = new C(str);
+		P p = new P(str);
+		ThreadP tp = new ThreadP(p);
+		ThreadR ct = new ThreadR(c);
+		tp.setName("pt ");
+		ct.setName("ct ");
+		tp.start();
+		ct.start();
 	}
 
 }
